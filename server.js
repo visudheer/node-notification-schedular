8af3384e8391d3f2dc2d413339ab450d7fd92f0c
Sudheer5V <sudheerbannu9@gmail.com>
2023-04-29 16:25:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { scheduleJob } from "node-schedule";
+import * as dotenv from "dotenv";
+import axios from "axios";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+//"0 6 * * *"
+
+const job = scheduleJob("*/3 * * * * *", async () => {
+  await axios
+    .get(process.env.APOD_API + process.env.APOD_API_KEY)
+    .then((data) => {
+      axios
+        .post(process.env.DB_URL + process.env.DB_PATH, {
+          title: data.data.title,
+          explanation: data.data.explanation,
+          date: data.data.date,
+          imgUrl: data.data.hdurl,
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(5000);
